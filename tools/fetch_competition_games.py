0f5c65cc1964795acc7754dbbe1bef32f0c838f8
Peter Smit <peter@smitmail.eu>
2014-10-20 12:20:35 +0300

    Finished fetching script

--- tools/fetch_competition_games.py
+++ tools/fetch_competition_games.py
@@ -1,23 +1,25 @@
+import html
 import re
 import sys
 from urllib.request import Request, urlopen
 
 __author__ = 'psmit'
 
-def get_log(base_url, id, cookie):
-    request = Request(base_url + id, headers={'Cookie': cookie})
+def get_log(base_url, gameid, cookie):
+    request = Request(base_url + gameid, headers={'Cookie': cookie})
     response = urlopen(request)
     body = response.read().decode('utf-8')
 
     log = None
     for line in body.splitlines():
-        if line.starswith("<b>Player messages:</b>"):
-            log = 1
-        if line.starswith("<b>Checker messages:</b><br>You&nbsp;have&nbsp;been&nbsp;disqualified"):
+        if line.startswith("<b>Player messages:</b><br>"):
+            log = html.unescape(line[len("<b>Player messages:</b><br>"):]).split("<br>")
+        if line.startswith("<b>Checker messages:</b><br>You&nbsp;have&nbsp;been&nbsp;disqualified"):
             log = None
 
     if log:
-        pass
+        with open('../games/{}.log'.format(gameid), 'w') as out_f:
+            out_f.write("\n".join(log))
 
 def main(competition_url, cookie):
     request = Request(competition_url, headers={'Cookie': cookie})
@@ -27,7 +29,7 @@ def main(competition_url, cookie):
     regex = re.compile("showgame.php\?ga=([0-9]+)")
 
     for match in regex.findall(body):
-        get_log('http://www.codecup.nl/showgame.php?ga=', id, cookie)
+        get_log('http://www.codecup.nl/showgame.php?ga=', match, cookie)
 
 
 if __name__ == "__main__":
