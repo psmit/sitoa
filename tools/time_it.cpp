4ab7abdfbee6a83712f2c414da33bf26676f63e3
Peter Smit <peter@smitmail.eu>
2014-11-18 13:59:24 +0200

    time it

--- tools/time_it.cpp
+++ tools/time_it.cpp
@@ -115,14 +115,19 @@ void game_loop(FILE *fp) {
             );
             fflush(stdout);
             depth++;
+            if (STORAGE_ID * 2 > STORAGE_SIZE) {
+                fprintf(stderr, "#Clearing transposition table %u \n", STORAGE_ID);
+                clear_transposition_table();
+                fflush(stderr);
+            }
         }
 
 
-        if (STORAGE_ID * 2 > STORAGE_SIZE) {
-            fprintf(stderr, "#Clearing transposition table %u \n", STORAGE_ID);
-            clear_transposition_table();
-            fflush(stderr);
-        }
+//        if (STORAGE_ID * 2 > STORAGE_SIZE) {
+//            fprintf(stderr, "#Clearing transposition table %u \n", STORAGE_ID);
+//            clear_transposition_table();
+//            fflush(stderr);
+//        }
 //        statistics.pause();
 
 //        puts(out);
