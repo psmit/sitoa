3264323e64e91be5d6e18d203711636945748c96
Peter Smit <peter@smitmail.eu>
2014-11-18 13:47:46 +0200

    time_it

--- tools/time_it.cpp
+++ tools/time_it.cpp
@@ -1,21 +1,17 @@
 #include "headers.h"
-#include "time.h"
 
-#define XSTR(x) STR(x)
-#define STR(x) #x
+typedef std::chrono::high_resolution_clock my_clock;
 
-#pragma message "Clock resolution: " XSTR(CLOCKS_PER_SEC)
-
-double time_negamax(search_node *node, int depth, board_t *move) {
+long time_negamax(search_node *node, int depth, board_t *move) {
     int score;
-    clock_t t;
 
-    t = clock();
+    auto start = my_clock::now();
+
+
     *move = negamax_memory_decision(*node, depth, &score);
-    t = clock() - t;
 
-    return ((double) t) / CLOCKS_PER_SEC;
 
+    return std::chrono::duration_cast<std::chrono::milliseconds>(my_clock::now()-start).count();
 }
 
 
@@ -48,6 +44,8 @@ void game_loop(FILE *fp) {
 //        statistics.resume();
         if (read_quit(line)) {
             break;
+        } else if (strlen(line) < 3) {
+            continue;
         } else if (read_comment(line) || read_debug(line, &debug)) {
             continue;
         } else if (read_logstring(line, &sn, &depth, &score)) {
@@ -62,8 +60,8 @@ void game_loop(FILE *fp) {
         // ply, #moves, depth, sol_dis_me, sol_dis_other, sol_score, time_cur, time_cur+1, time_cur+2, #moves_nextply, next-1, next-0, next+1, next+2
 
         int max_iter = 4;
-        double base_time = 0.0;
-        while (base_time < 1.0 && max_iter > 0) {
+        long base_time = 0l;
+        while (base_time < 1000l && max_iter > 0) {
             max_iter--;
 
             num_moves = sn_find_moves(&sn, moves);
@@ -72,19 +70,17 @@ void game_loop(FILE *fp) {
             int score_tot;
             sn_scores(&sn, &score_me, &score_ot, &score_tot);
 
-            clear_transposition_table();
+
             base_time = time_negamax(&sn, depth, &move);
-            if (base_time < 0.05) {
+            if (base_time < 50l && max_iter > 1) {
                 depth++;
                 continue;
             }
-            double times[7];
+
+            double times[2];
             int i;
 
-            for (i = 1; i < 3; ++i) {
-                clear_transposition_table();
-                times[i] = time_negamax(&sn, depth + i, &move) / base_time;
-            }
+
 
             sn2 = sn_apply_move(sn, move);
             sn_find_moves(&sn2, moves);
@@ -96,42 +92,37 @@ void game_loop(FILE *fp) {
             num_moves_n = sn_find_moves(&sn2, moves);
 
 
-            for (i = -1; i < 3; ++i) {
-                clear_transposition_table();
-                times[i + 4] = time_negamax(&sn2, depth + i, &move) / base_time;
+            for (i = 0; i < 2; ++i) {
+                times[i] = time_negamax(&sn2, depth + i, &move) / (double)base_time;
             }
 
-            fprintf(stderr, "%.4f\n", base_time);
+            fprintf(stderr, "%ld\n", base_time);
             fflush(stderr);
 
-            printf("%d %d %d %d %d %d %.2f %.2f %d %d %d %d %.2f %.2f %.2f %.2f\n",
+            printf("%d %d %d %d %d %d %d %d %d %d %.2f %.2f\n",
                     sn.ply,
                     depth,
                     num_moves,
                     score_me,
                     score_ot,
                     score_tot,
-                    times[1],
-                    times[2],
                     num_moves_n,
                     score_me_n,
                     score_ot_n,
                     score_tot_n,
-                    times[3],
-                    times[4],
-                    times[5],
-                    times[6]
+                    times[0],
+                    times[1]
             );
             fflush(stdout);
             depth++;
         }
 
 
-//        if (STORAGE_ID * 2 > STORAGE_SIZE) {
-//            clear_transposition_table();
-//            fputs("#Clearing transposition table", stderr);
-//            fflush(stderr);
-//        }
+        if (STORAGE_ID * 2 > STORAGE_SIZE) {
+            clear_transposition_table();
+            fputs("#Clearing transposition table\n", stderr);
+            fflush(stderr);
+        }
 //        statistics.pause();
 
 //        puts(out);
@@ -142,9 +133,6 @@ void game_loop(FILE *fp) {
 }
 
 int main(int argc, const char *argv[]) {
-
-    init_stats();
-
     init_rand(0);
 
     FILE *fp = stdin;
@@ -156,8 +144,5 @@ int main(int argc, const char *argv[]) {
     if (argc > 1) {
         fclose(fp);
     }
-
-    cleanup_stats();
-
     return 0;
 }
